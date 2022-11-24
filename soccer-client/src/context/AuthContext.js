b2e7c7be2bbc7897c47160677dacebcb9ec5e8bc
xisto eugenio <xistoeugeniosilva@gmail.com>
2022-11-23 23:02:41 -0300

    created AuthContext

--- soccer-client/src/context/AuthContext.js
+++ soccer-client/src/context/AuthContext.js
@@ -1,55 +1,27 @@
-import { createContext, useReducer } from "react";
+import { createContext, useEffect, useState } from "react";
 
-const INITIAL_STATE = {
-    user: null,
-    loading: false,
-    error: null
-};
+export const AuthContext = createContext();
 
-export const AuthContext = createContext(INITIAL_STATE);
+export const AuthContextProvider = ({ children }) => {
 
-const AuthReducer = (state, action) => {
-    switch (action.type) {
-        case "LOGIN_START":
-            return {
-                user: null,
-                loading: true,
-                error: null
-            };
-        case "LOGIN_SUCCESS":
-            return {
-                user: action.payload,
-                loading: false,
-                error: null
-            };
-        case "LOGIN_FAILURE":
-            return {
-                user: null,
-                loading: false,
-                error: action.payload
-            };
-        case "LOGOUT":
-            return {
-                user: null,
-                loading: false,
-                error: null,
-            };
-        default:
-            return state;
+    const [currentUser, setCurrent] = useState(
+        JSON.parse(localStorage.getItem("user")) || null
+    )
+
+    const login = ()=>{
+
     }
-};
 
-export const AuthProvider = ({ children }) => {
-    const [state, dispatchAuth] = useReducer(AuthReducer, INITIAL_STATE);
+    useEffect(()=>{
+        localStorage.setItem("user", JSON.stringify(currentUser));
+    },[currentUser])
 
 
     return (
         <AuthContext.Provider
             value={{
-                user: state.user,
-                loading: state.loading,
-                error: state.error,
-                dispatchAuth
+                currentUser,
+                login
             }}
         >
             {children}
--- soccer-client/src/index.js
+++ soccer-client/src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { GameProvider } from './context/gameContext';
 import { SinglePlayerProvider } from './context/SinglePlayerContext';
+import { AuthContextProvider } from './context/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <GameProvider>
       <SinglePlayerProvider>
-        <App />
+        <AuthContextProvider>
+          <App />
+        </AuthContextProvider>
       </SinglePlayerProvider>
     </GameProvider>
   </React.StrictMode>
