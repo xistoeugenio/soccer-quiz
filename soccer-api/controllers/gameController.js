1dcbd5ac41ef46da03329c0ab3f61472aaeec799
Xisto Eugenio <xistoeugeniosilva@gmail.com>
2022-12-01 14:17:16 -0300

    created ramdomOptions function in  gameCrontroller

--- soccer-api/controllers/gameController.js
+++ soccer-api/controllers/gameController.js
@@ -1,25 +1,66 @@
 import Player from "../models/Player.js";
 import Match from "../models/Match.js";
 
-const randomNumber = (number, number2, number3) => {
-    var ranNum = Math.floor(Math.random() * 20 + 1);
-
-    if (number === ranNum || number2 === ranNum || number3 === ranNum) {
-        while (number === ranNum || number2 === ranNum || number3 === ranNum) {
-            ranNum = Math.floor(Math.random() * 20 + 1)
-        }
-    }
-
-    return ranNum
-}
-
 const drawAnswer = () => {
     var random = Math.floor(Math.random() * 4);
 
     return random
 }
 
+const randomOptions = (value) => {
 
+    //this part is responsible to drawing 4 players from the object received as a parameter
+    const numbers = []
+    while (numbers.length < 4) {
+        var aleatorio = Math.floor(Math.random() * value.length);
+
+        if (numbers.indexOf(aleatorio) == -1)
+            numbers.push(aleatorio);
+    }
+
+    const players = []
+    numbers.map(number => {
+        players.push(value[number])
+    })
+
+
+    //here we draw 1 of the four players to be the correct answer
+    const currentPlayer = players[drawAnswer()]
+
+    const options = players.map(item => ({ name: item.name, id: item.id }))
+
+    const { id, team, country, league, position, name } = currentPlayer
+
+
+    //here we return the correct format to be placed in the database
+    const newMatch = {
+        options: options,
+        info: { team, country, league, position },
+        rightAnswer: { id, name }
+    }
+
+
+    return newMatch
+}
+
+export const startMatch = async (req, res, next) => {
+    const { mode } = req.query
+    var data = null
+    try {
+        if (mode === "brazilian") {
+            data = await Player.find({ "country": "Brazil" })
+        } else {
+            data = await Player.find()
+        }
+
+        return res.status(200).json(randomOptions(data))
+    } catch (error) {
+        next(error)
+    }
+
+}
+
+/*
 export const startMatch = async (req, res, next) => {
 
     try {
@@ -55,19 +96,19 @@ export const startMatch = async (req, res, next) => {
     } catch (err) {
         next(err)
     }
-}
+}*/
 
 export const verifyAnswer = async (req, res, next) => {
 
     const { player_id, match_id } = req.query;
-    
+
     try {
-        
+
         const currentMacth = await Match.findById(match_id)
         var response = null
 
         if (currentMacth.rightAnswer.id === player_id) {
-            response  = "right"
+            response = "right"
         } else {
             response = "wrong"
         }
