71d929fde6e26803de43e7c85afd0c0a683d26a6
Xisto Eugenio <xistoeugeniosilva@gmail.com>
2022-12-07 08:11:44 -0300

    finished AboutUs page

--- soccer-client/src/pages/aboutUs/AboutUs.jsx
+++ soccer-client/src/pages/aboutUs/AboutUs.jsx
@@ -9,8 +9,8 @@ export default function AboutUs() {
         </div>
         <div className="textTopContainer">
           <p>
-            This game was created by just one developer 
-            called <a href="https://xisto.xyz">Xisto Eugênio</a>.
+            This game was created by just one developer
+            called <a href="https://xisto.xyz" target="blank">Xisto Eugênio</a>.
             This is an open source game developed on the MERN stack that aims
             to make everyone know soccer players.
           </p>
@@ -18,12 +18,16 @@ export default function AboutUs() {
       </div>
       <div className="aboutBottomContainer">
         <div className="infoContainer">
-          <h2>Source Code</h2>
+          <h2>Developer site</h2>
+          <a href="https://xisto.xyz" target="blank">www.xisto.xyz</a>
+          <h2>Linkedin profile</h2>
+          <a href="https://www.linkedin.com/in/xisto-eug%C3%AAnio-266029171/" target="blank">
+            linkedin.com/in/xisto-eugênio
+          </a>
+          <h2>Source code</h2>
           <a href="https://github.com/xistoeugenio/soccer-quiz" target="blank">
             github.com/xistoeugenio/socciz
           </a>
-          <h2>Developer Site</h2>
-          <a href="https://xisto.xyz">www.xisto.xyz</a>
         </div>
         <div className="textBottomContainer">
           <p>
@@ -31,7 +35,7 @@ export default function AboutUs() {
             If you want to participate in the development of this amazing game you
             can contribute with your feedback.
           </p>
-          <a href="https://xisto.xyz" target="blank">Feedback</a>
+          <a href="mailto:xistoeugeniosilva1@gmail.com" target="blank">Feedback</a>
         </div>
       </div>
     </div>
